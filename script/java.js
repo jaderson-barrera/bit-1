78b21b7aef0524f5e1105ca5be530f2c01b57f01
jaderson barrera <perezbarrera098@gmail.com>
2025-05-12 15:47:56 -0500

    se modifico javaScript

--- script/java.js
+++ script/java.js
@@ -1,1 +1,7 @@
-console.log("holaaaaaaaaaaa");
+'use strict';
+
+const boton = document.querySelector('button')
+
+boton.addEventListener('click', function() {
+    alert('Gracias por registrate')
+})
